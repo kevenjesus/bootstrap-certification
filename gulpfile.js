4a0e0c0c7abcf8dcaaaf17eb04ea41ee7e64e4cc
APSantos-Pires <ceo@apsantos.com.br>
2017-04-14 14:05:39 -0300

    creating tasks in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
-var gulp = require("gulp");
+var gulp = require('gulp');
+var del = require('del');
 
-gulp.task('move-css', function() {
+gulp.task('clean', function() {
+  del('./dist/css');
+});
+
+gulp.task('move-css',['clean'], function() {
   return gulp.src('./source/css/style.css')
          .pipe(gulp.dest('./dist/css'));
 });
@@ -9,3 +14,8 @@ gulp.task('move-js', function() {
   return gulp.src('./source/js/index.js')
          .pipe(gulp.dest('./dist/js'));
 });
+
+gulp.task('background', function() {
+  gulp.watch('./source/css/style.css',['move-css']);
+  gulp.watch('./source/js/index.js',['move-js']);
+})
