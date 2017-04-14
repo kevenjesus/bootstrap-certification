ee8b661473b5d9e34c34efb2bd8afe9025fdc24c
APSantos-Pires <ceo@apsantos.com.br>
2017-04-14 19:41:35 -0300

    created task: layout

--- assets/activities/practical-activity-workflow/gulpfile.js
+++ assets/activities/practical-activity-workflow/gulpfile.js
@@ -17,7 +17,15 @@ gulp.task('base', function () {
     .pipe(gulp.dest('./dist/css/'))
 });
 
+// monitors file changes: layout.scss
+gulp.task('layout', function () {
+  return gulp.src('./source/scss/layout.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css/'))
+});
+
 // runs the tasks automatically
 gulp.task('sass:watch', function () {
   gulp.watch('./source/scss/base.scss', ['base']);
+  gulp.watch('./source/scss/layout.scss', ['layout']);
 })
