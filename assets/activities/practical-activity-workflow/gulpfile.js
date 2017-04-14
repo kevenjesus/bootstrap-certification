0ef4c4378ebc064b623470551f090dcc91c5ab05
APSantos-Pires <ceo@apsantos.com.br>
2017-04-14 19:35:41 -0300

    created task: base

--- assets/activities/practical-activity-workflow/gulpfile.js
+++ assets/activities/practical-activity-workflow/gulpfile.js
@@ -3,8 +3,21 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+// task to minify .scss files
 gulp.task('sass', function () {
   return gulp.src('./source/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist/css/'));
 });
+
+// monitors file changes: base.scss
+gulp.task('base', function () {
+  return gulp.src('./source/scss/base.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css/'))
+});
+
+// runs the tasks automatically
+gulp.task('sass:watch', function () {
+  gulp.watch('./source/scss/base.scss', ['base']);
+})
