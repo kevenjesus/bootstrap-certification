25230368b1e2d0f9cda5e70d7f1ae4791845ac5b
APSantos-Pires <ceo@apsantos.com.br>
2017-04-14 19:19:38 -0300

    install gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,23 @@
+'use strict';
+
 var gulp = require('gulp');
 var del = require('del');
 
-gulp.task('clean', function() {
+gulp.task('clean', function () {
   del('./dist/css');
 });
 
-gulp.task('move-css',['clean'], function() {
+gulp.task('move-css',['clean'], function () {
   return gulp.src('./source/css/style.css')
          .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('move-js', function() {
+gulp.task('move-js', function () {
   return gulp.src('./source/js/index.js')
          .pipe(gulp.dest('./dist/js'));
 });
 
-gulp.task('background', function() {
+gulp.task('background', function () {
   gulp.watch('./source/css/style.css',['move-css']);
   gulp.watch('./source/js/index.js',['move-js']);
 })
