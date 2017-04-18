644feced6e6bd5b2122dba86058daf283225f9d4
APSantos-Pires <ceo@apsantos.com.br>
2017-04-18 10:54:29 -0300

    file changed: gulpfile.js

--- assets/activities/practical-activity-sass/gulpfile.js
+++ assets/activities/practical-activity-sass/gulpfile.js
@@ -3,24 +3,23 @@
 'use strict';
 
 // plugins
-const gulp    = require('gulp')
-    , sass    = require('gulp-sass')
-    , notify  = require('gulp-notify')
+const gulp    = require( 'gulp' )
+    , sass    = require( 'gulp-sass' )
+    , notify  = require( 'gulp-notify' )
 ;
 
 // task to compile style.scss files
-gulp.task('sass', function(){
-  // return gulp.src(['./sass/*.sass','./scss/*.scss'])
-  return gulp.src('./scss/_style.scss')
-    .pipe(sass())
-    .on('error', notify.onError({title:'erro ao compilar', message:'<%= error.message %>'}))
-    .pipe(gulp.dest('./css'))
+gulp.task( 'sass', function() {
+  return gulp.src( './scss/_style.scss' )
+    .pipe( sass() )
+    .on( 'error', notify.onError( { title:'erro ao compilar', message:'<%= error.message %>' } ) )
+    .pipe( gulp.dest( './css' ) )
 });
 
 // task watch
-gulp.task('sass:watch', function(){
-  gulp.watch('./scss/**/*.scss', ['sass']);
+gulp.task( 'sass:watch', function() {
+  gulp.watch( './scss/**/*.scss', [ 'sass' ] );
 });
 
 // task default
-gulp.task('default', ['sass', 'sass:watch']);
+gulp.task( 'default', [ 'sass', 'sass:watch' ] );
