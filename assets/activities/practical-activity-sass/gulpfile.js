6bc27a52c7933f9b98e3ec8c78e1ce35db2249d3
APSantos-Pires <ceo@apsantos.com.br>
2017-04-18 10:47:40 -0300

    created file: gulpfile.js

---
+++ assets/activities/practical-activity-sass/gulpfile.js
@@ -0,0 +1,26 @@
+// comma-first style
+
+'use strict';
+
+// plugins
+const gulp    = require('gulp')
+    , sass    = require('gulp-sass')
+    , notify  = require('gulp-notify')
+;
+
+// task to compile style.scss files
+gulp.task('sass', function(){
+  // return gulp.src(['./sass/*.sass','./scss/*.scss'])
+  return gulp.src('./scss/_style.scss')
+    .pipe(sass())
+    .on('error', notify.onError({title:'erro ao compilar', message:'<%= error.message %>'}))
+    .pipe(gulp.dest('./css'))
+});
+
+// task watch
+gulp.task('sass:watch', function(){
+  gulp.watch('./scss/**/*.scss', ['sass']);
+});
+
+// task default
+gulp.task('default', ['sass', 'sass:watch']);
